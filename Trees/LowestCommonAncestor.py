0e3089244964fae8f7630def9dcd38f8c5333c05
ArthurWalker <whitefang17996@gmail.com>
2025-07-09 00:13:22 +0100

    Explaination of LowestCommonANcestor

--- Trees/LowestCommonAncestor.py
+++ Trees/LowestCommonAncestor.py
@@ -5,16 +5,37 @@
 #         self.left = left
 #         self.right = right
 
+# The key is that if both p and q less than root then we will search on the left
+# while if both p and q more than root then we will search on the right
+# If not above cases then the search will be splitted in at root and root is the common ancestor
+# Edge case is even when p and q == root then root is still the common ancestor
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+#         if not root or not q or not p:
+#             return None
+        
+#         curr = root
+#         while curr:
+#             if curr.val < p.val and curr.val < q.val:
+#                 curr = curr.right
+#             elif curr.val > p.val and curr.val > q.val:
+#                 curr = curr.left
+#             else:
+#                 return curr
+
+
+# Recursion:
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         if not root or not q or not p:
             return None
         
-        curr = root
-        while curr:
-            if curr.val < p.val and curr.val < q.val:
-                curr = curr.right
-            elif curr.val > p.val and curr.val > q.val:
-                curr = curr.left
-            else:
-                return curr
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return root
+
+
