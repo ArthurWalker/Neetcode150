f35382025692989580cf3d6f083c0158e0de2b53
ArthurWalker <whitefang17996@gmail.com>
2025-07-09 00:02:10 +0100

    Another Binary tree exercises

---
+++ Trees/LowestCommonAncestor.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or not q or not p:
+            return None
+        
+        curr = root
+        while curr:
+            if curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            else:
+                return curr
