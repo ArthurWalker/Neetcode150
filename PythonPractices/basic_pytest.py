6ac356dba0e477b872d3c742f51e16e0259e7ae1
ArthurWalker <whitefang17996@gmail.com>
2025-07-11 17:48:38 +0100

    complete pytest

--- PythonPractices/basic_pytest.py
+++
@@ -1,8 +0,0 @@
-def get_weather(temp):
-    if temp >= 20:
-        return "hot"
-    else:
-        return "cold"
-
-if __name__ == '__main__':
-    print(get_weather(20))
--- PythonPractices/test_basic.py
+++ PythonPractices/test_basic.py
@@ -1,7 +1,20 @@
-from basic_pytest import get_weather
+from basic_function import get_weather, add, divide
+import pytest
 
 def test_weather_hot():
     assert get_weather(20)=='hot', 'The temperature is colder'
 
 def test_weather_cold():
-    assert get_weather(20)=='cold', 'The temperature is colder'
+    assert get_weather(10)=='cold', 'The temperature is colder'
+
+def test_add():
+    assert add(2,3) == 5, '2+3 should be 5'
+    assert add(-1,1)==0, '-1+1 should be 0'
+    assert add(0,0)==0, '0+0 should be 0'
+
+def test_divide_failed():
+    with pytest.raises(ValueError, match='Cannot divide by zero'):
+        divide(10,0)
+
+def test_divide_well():
+    assert divide(10,1)==10, '10/1 should be 10'
