617ab6261459f90d57ab795bf6b24849e6c929c3
ArthurWalker <whitefang17996@gmail.com>
2025-07-13 20:17:33 +0100

    Add Heap approaches

--- Heap_PriorityQueue/KClosetPoints.py
+++ Heap_PriorityQueue/KClosetPoints.py
@@ -1,13 +1,30 @@
-class Solution:
-    def distance(self,x,y):
-        from math import sqrt
-        squrr = sqrt(x**2+y**2)
-        return squrr
+class KthLargest:
 
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        dis_point = [[self.distance(point[0],point[1]),point] for point in points]
-        print(dis_point)
-        dis_point.sort(key = lambda x: x[0])
-        k_items = dis_point[:k]
-        print(k_items)
-        return [val[1] for val in k_items]
+    def __init__(self, k: int, nums: List[int]):
+        self.k_max = k
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+        # Remove (n-k) min elements to keep only the k largest items currently
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)  # Remove top item (which is the smallest) of self.minHeap
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap,val) # Push an item to self.minHeap and rearrange
+        if len(self.minHeap) > self.k_max:
+            heapq.heappop(self.minHeap) # Remove to maintain top k items
+        return self.minHeap[0]
+
+
+# class Solution:
+#     def distance(self,x,y):
+#         from math import sqrt
+#         squrr = sqrt(x**2+y**2)
+#         return squrr
+
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         dis_point = [[self.distance(point[0],point[1]),point] for point in points]
+#         print(dis_point)
+#         dis_point.sort(key = lambda x: x[0])
+#         k_items = dis_point[:k]
+#         print(k_items)
+#         return [val[1] for val in k_items]
--- Heap_PriorityQueue/LastStoneWeight.py
+++ Heap_PriorityQueue/LastStoneWeight.py
@@ -2,15 +2,33 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         if len(stones) == 1:
             return stones[0]
-        stones.sort()
-        stones = stones[::-1]
-        while len(stones) > 1:
-            remain = stones[0]
-            subs = abs(remain - stones[1])
-            if len(stones[2:]):
-                stones = [subs] + stones[2:]
-            else:
-                stones = [subs]
-            stones.sort()
-            stones = stones[::-1]
-        return stones[-1]
+       
+        new_stones = [-1 *i for i in stones]
+        heapq.heapify(new_stones)
+        while len(new_stones)>1:
+            print(new_stones)
+            top_val1 = heapq.heappop(new_stones)
+            top_val2 = heapq.heappop(new_stones)
+            diff = top_val1-top_val2
+            if top_val2 > top_val1:
+                heapq.heappush(new_stones,top_val1-top_val2)
+        new_stones.append(0)
+        return abs(new_stones[0])
+
+
+# class Solution:
+#     def lastStoneWeight(self, stones: List[int]) -> int:
+#         if len(stones) == 1:
+#             return stones[0]
+#         stones.sort()
+#         stones = stones[::-1]
+#         while len(stones) > 1:
+#             remain = stones[0]
+#             subs = abs(remain - stones[1])
+#             if len(stones[2:]):
+#                 stones = [subs] + stones[2:]
+#             else:
+#                 stones = [subs]
+#             stones.sort()
+#             stones = stones[::-1]
+#         return stones[-1]
