e5a1742416760c90e517517cdf33b54bbd34e92d
ArthurWalker <whitefang17996@gmail.com>
2025-07-13 20:43:08 +0100

    Update K Largest Ele Array SOlution

--- Heap_PriorityQueue/KLargestEleArray.py
+++ Heap_PriorityQueue/KLargestEleArray.py
@@ -1,4 +1,17 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums) - k]
+        if len(nums) == 1:
+            return nums[0]
+        new_nums = [-num for num in nums]
+        heapq.heapify(new_nums)
+        while k > 0:
+            temp_res = -1*heapq.heappop(new_nums)
+            print(temp_res)
+            k-=1
+        
+        return temp_res
+
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         nums.sort()
+#         return nums[len(nums) - k]
