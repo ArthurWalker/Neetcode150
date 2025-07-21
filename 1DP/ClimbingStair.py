7bfc6ea87b90094b4bb1162a2179e9dd9c300599
ArthurWalker <whitefang17996@gmail.com>
2025-07-21 21:50:38 +0100

    Update DP Top down process

--- 1DP/ClimbingStair.py
+++ 1DP/ClimbingStair.py
@@ -8,3 +8,22 @@ class Solution:
         for i in range(3,n+1):
             dp[i] = dp[i-1]+dp[i-2]
         return dp[n]
+    
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n <= 2:
+#             return n
+
+#         memo = [-1]*n  # Create flag to say if it passes this
+#         def dfs(num):  
+#             if num > n:
+#                 return 0
+#             if num == n:
+#                 return 1
+#             if memo[num] != -1: # if passing this point
+#                 return memo[num] # then dont need to go further and return the result at that position
+
+#             memo[num] = dfs(num+1)+dfs(num+2)
+#             return memo[num]
+        
+#         return dfs(0)
--- 1DP/HouseRobber.py
+++ 1DP/HouseRobber.py
@@ -14,3 +14,24 @@ class Solution:
             dp[i] = max(dp[i-1],nums[i]+dp[i-2])
 
         return dp[-1]
+    
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        if len(nums) == 1:
+            return nums[0]
+
+        memo = [-1]*len(nums)
+        def dfs(i):
+            if i >=len(nums):
+                return 0
+            
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(nums[i]+dfs(i+2),dfs(i+1)) # Rob this house and next house nums[i]+dfs(i+2)
+                                                # or from next houses dfs(i+1)
+
+            return memo[i]
+        return dfs(0)
